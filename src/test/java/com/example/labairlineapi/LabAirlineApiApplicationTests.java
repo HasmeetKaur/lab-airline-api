e5e45ab8390c528ea0dbd9d52aa726cb521b708e
Hasmeet <hasmeetkaur.hk@gmail.com>
2022-09-09 11:11:39 +0100

    initial commit

---
+++ src/test/java/com/example/labairlineapi/LabAirlineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labairlineapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabAirlineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
